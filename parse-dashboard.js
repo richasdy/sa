c75715161bc52de63514feb03384311c9f072414
Donni Richasdy <donni.richasdy@gmail.com>
2016-12-08 11:36:04 +0700

    configuration

---
+++ parse-dashboard.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var ParseDashboard = require('parse-dashboard');
+
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": "http://localhost:1337/parse",
+      "appId": "myAppId",
+      "masterKey": "myMasterKey",
+      "appName": "MyApp"
+    }
+  ]
+});
+
+var app = express();
+
+// make the Parse Dashboard available at /dashboard
+app.use('/dashboard', dashboard);
+
+var httpServer = require('http').createServer(app);
+httpServer.listen(4040);
