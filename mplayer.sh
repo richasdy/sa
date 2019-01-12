71b2c084631af5986dc42494c52123d87d6f7ce3
richasdy <donni.richasdy@gmail.com>
2019-01-12 14:58:33 +0700

    mplayer

---
+++ mplayer.sh
@@ -0,0 +1,31 @@
+# radio rodja
+mplayer http://live.radiorodja.com
+mplayer http://live.radiorodja.com:80/;stream.nsv
+#error
+mplayer http://live2.radiorodja.com/listen.pls
+#error
+mplayer http://live.radiorodja.com/listen.pls
+#error
+mplayer http://rodja.sytes.net
+
+
+# radio attaqwa
+mplayer http://i.klikhost.com:8840/listen.pls
+
+# Radio Muslim
+http://live.radiomuslim.com:8024
+
+# Radio Hang
+http://radiohang.sytes.net
+
+# Radio Assunnah
+http://live.radioassunnah.com:8020
+
+# Suara Quran
+http://suara-quran.sytes.net:8022
+
+# Telaga Hati Ust. Abu Zubair
+http://telaga-hati.sytes.net:8020
+
+# Ngaji Online
+http://ngaji-online.sytes.net:8006
