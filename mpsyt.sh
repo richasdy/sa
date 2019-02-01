0d09e21445ab06f89c1562e6ae9130ea35053ccf
richasdy <donni.richasdy@gmail.com>
2019-02-01 10:38:57 +0700

    add mpsyt

--- mpsyt.sh
+++ mpsyt.sh
@@ -5,3 +5,10 @@ mpsyt url ugXrDEeUWmE
 mpsyt playurl ugXrDEeUWmE
 # direct download
 mpsyt dlurl ugXrDEeUWmE
+
+# my playlist for imam
+mpsyt playurl https://www.youtube.com/playlist?list=PLAc6B3ulGMsGm9wha4arp6xD-onWAQS1d
+mpsyt playurl PLAc6B3ulGMsGm9wha4arp6xD-onWAQS1d
+
+# playlist ust nouman quran weekly, miracle quran
+mpsyt playurl PLFP6-6Ao4FMm5FnIT9QvstEmhWNugJlNO
