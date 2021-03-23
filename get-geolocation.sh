2be84c5649b57e712f5cec2ed7c079b892570721
richasdy <donni.richasdy@gmail.com>
2021-03-24 06:56:50 +0700

    add tmux

---
+++ get-geolocation.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+curl https://ipinfo.io/ip
+curl https://ipinfo.io/ip | curl https://ipvigilante.com/
+curl https://ipinfo.io/ip | curl http://api.geoiplookup.net/?query=
+curl https://ipinfo.io/ip | curl curl https://json.geoiplookup.io/
---
+++ get-hardware.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+lscpu
+lshw
+hwinfo
+lspci
+lspci
+lsscsi
+lsusb
+inxi -Fx
+lsblk
+df -H
+pydf
+sudo fdisk -l
+mount | column -t | grep ext
+free -m
