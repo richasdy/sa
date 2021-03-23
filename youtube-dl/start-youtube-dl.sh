40cee0f93eb18f1ad871c632de530a092fe720a6
richasdy <donni.richasdy@gmail.com>
2021-03-24 06:55:46 +0700

    add youtube

--- youtube-dl/start-youtube-dl.sh
+++ youtube-dl/start-youtube-dl.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-youtube-dl --extract-audio --audio-format mp3 --audio-quality 5 --batch-file start-youtube-dl-list.txt
+# youtube-dl --extract-audio --audio-format mp3 --audio-quality 5 --batch-file start-youtube-dl-list.txt
+youtube-dl --extract-audio --audio-format mp3 --audio-quality 5 --batch-file start-youtube-dl-list-tazkiyatun-nufus.txt
