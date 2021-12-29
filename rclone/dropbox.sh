864b6f3ba60fe043208df328b0b6fb2bc19efb8f
richasdy <donni.richasdy@gmail.com>
2021-12-29 05:58:25 -0500

    add rclone cloud storage

---
+++ rclone/dropbox.sh
@@ -0,0 +1,32 @@
+# rclone command
+rclone \
+mount \
+--verbose \
+--progress \
+--buffer-size 32M \
+--vfs-cache-mode full \
+--vfs-read-chunk-size 32M \
+--vfs-read-chunk-size-limit 10G \
+--fast-list \
+--cache-dir ~/.dropbox \
+dropbox: ~/dropbox
+
+
+
+# cache location
+#--cache-dir ~/.gdrive-tu \
+
+# doubles the receive buffer to reduce buffering issues.
+#--buffer-size 32M \
+
+# for watching movie sequencial read
+#--max-read-ahead 200M \
+
+#--progress \
+#--progress-terminal-title \
+
+#--daemon \
+#--log-file ~/.rclone.log \ # not found
+#--log-level INFO \
+
+
---
+++ rclone/rclone-example.sh
@@ -0,0 +1,31 @@
+# rclone command
+rclone mount \
+--vfs-cache-mode full \
+--fast-list \
+--cache-dir ~/.gdrive-tu \
+gdrive-tu: ~/gdrive-tu \
+
+
+
+# cache location
+#--cache-dir ~/.gdrive-tu \
+
+# buffer
+#--buffer-size 32M \
+
+# for wathing movie sequencial read
+#--max-read-ahead 200M \
+
+# fast list for directory
+#--fast-list \
+
+# cache mode
+#--vfs-cache-mode full \
+#--vfs-cache-max-age 1h \
+#--vfs-read-chunk-size 10M \
+
+#--daemon \
+#--log-file ~/.rclone.log \ # not found
+#--log-level INFO \
+
+
